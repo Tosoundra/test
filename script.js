1c973fb59a1f4e7e59b4d1e41aefa4c6423a1db0
Antony Kowalski <beheaded_96@mail.ru>
2023-02-26 12:01:03 +0300

    added script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let Name;
+
+function printName(Name) {
+    if(Name ==='') {
+        alert('Enter your Name');
+    } 
+    else {
+        alert("You are suck" + Name);
+    }
+}
+
+printName("John");
